5aee300dfa819ae647360c54c2098ebe5fab3d10
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-10-21 17:04:07 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,29 @@
+var nome = "Marcelo";
+var idade = 31;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Zanguettin",
+  cidade = "Pompéia";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// Exercicio proposto
+
+var comidaFavorita;
+
+comidaFavorita = "Comida Japonesa";
+
+var time, professor, pais, edereco, rua;
+
+console.log(nome, sobrenome, idade, comidaFavorita);
